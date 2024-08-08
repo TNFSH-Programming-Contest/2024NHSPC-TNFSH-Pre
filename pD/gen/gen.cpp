210743023bdbc29e036da01633da243f28f3b660
Samuel <138226245+GuoYuKai-SaMuEl@users.noreply.github.com>
2024-08-08 21:30:04 +0800

    Update gen.cpp

--- pD/gen/gen.cpp
+++ pD/gen/gen.cpp
@@ -1,14 +1,43 @@
-#include <bits/stdc++.h>
 #include "testlib.h"
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(int argc, char* argv[]) {
+int a[(int)2e5+5];
+int main(int argc, char* argv[]){
 	registerGen(argc, argv, 1);
-	// about testlib, see https://codeforces.com/blog/entry/18291
-
-	// TODO
-	int n = atoi(argv[1]);
-	cout << n << endl;
-
-	return 0;
+    int start_index = atoi(argv[1]);
+    int end_index = atoi(argv[2]);
+    int subtask=atoi(argv[3]);
+    for(int idx=start_index; idx<=end_index; idx++){
+        startTest(idx);
+        int n=(subtask==1?rnd.next(1000,2000):rnd.next(100000,200000)),x;
+        if(subtask==2||subtask==4||subtask==5)x=0;
+        else if(idx%3==0)x=rnd.next(1,1000);
+        else if(idx%3==1)x=rnd.next(1000,1000000);
+        else x=rnd.next(1000000,1000000000);
+        long long int y;
+        if(subtask==2||subtask==3)y=1e18;
+        else if(subtask==4)y=0;
+        else if(subtask==6)y=x;
+        else if(idx%4==0)y=rnd.next(1,10000);
+        else if(idx%4==1)y=rnd.next(10000,10000000);
+        else if(idx%4==2)y=rnd.next(10000000LL,10000000000LL);
+        else y=rnd.next(10000000000LL,1000000000000LL);
+        cout<<n<<' '<<x<<' '<<y<<'\n';
+        for(int i=0;i<n;i++){
+            if(idx%5==0)a[i]=rnd.next(1,100);
+            else if(idx%5==1)a[i]=rnd.next(1000,100000);
+            else if(idx%5==2)a[i]=rnd.next(100000,10000000);
+            else if(idx%5==3)a[i]=rnd.next(10000000,1000000000);
+            else a[i]=rnd.next(1,1000000000);
+        }
+        if(idx%6==0){
+            for(int i=0;i<n;i++)a[i]=-a[i];
+            a[rnd.next(n)]=1;
+        }else if(idx%6==1){
+            for(int i=0;i<n;i+=2)a[i]=-a[i];
+        }else if(idx%6==2){
+            for(int i=0;i<n;i+=3)a[i]=-a[i];
+        }
+        for(int i=0;i<n;i++)cout<<a[i]<<" \n"[i==n-1];
+    }
 }
