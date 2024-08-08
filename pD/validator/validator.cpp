5dbfdf48f7488f1c0b3f09b05f29d3a2c52ddf4e
Samuel <138226245+GuoYuKai-SaMuEl@users.noreply.github.com>
2024-08-08 22:08:30 +0800

    Update validator.cpp

--- pD/validator/validator.cpp
+++ pD/validator/validator.cpp
@@ -1,12 +1,21 @@
 #include "testlib.h"
+#include <bits/stdc++.h>
 using namespace std;
-
-const int MAXN = 1000;
 
 int main(int argc, char* argv[]) {
 	registerValidation(argc, argv);
-	// about testlib, see https://codeforces.com/blog/entry/18426
-
-	// TODO
+	int n=inf.readInt(1,2e5);
+	inf.readSpace();
+	inf.readInt(0,1e9);
+	inf.readSpace();
+	inf.readLong(0,1e18);
+	inf.readEoln();
+	
+	while(n--){
+		inf.readInt(-1e9,1e9);
+		if(n)inf.readSpace();
+		else inf.readEoln();
+	}
+	inf.readEof();
 	return 0;
 }
