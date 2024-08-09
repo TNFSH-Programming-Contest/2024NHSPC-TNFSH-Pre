6a51678e0942665f379f970d8aaab4f327c1dd2b
Samuel <138226245+GuoYuKai-SaMuEl@users.noreply.github.com>
2024-08-09 13:30:40 +0800

    Update gen.cpp

--- pF/gen/gen.cpp
+++ pF/gen/gen.cpp
@@ -1,14 +1,13 @@
-#include <bits/stdc++.h>
 #include "testlib.h"
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(int argc, char* argv[]) {
+int a[(int)2e5+5];
+int main(int argc, char* argv[]){
 	registerGen(argc, argv, 1);
-	// about testlib, see https://codeforces.com/blog/entry/18291
-
-	// TODO
-	int n = atoi(argv[1]);
-	cout << n << endl;
-
-	return 0;
+    int subtask=atoi(argv[1]);
+    if(subtask==1)cout<<rnd.next(100,10000000);
+    else if(subtask==2)cout<<rnd.next(10000000000LL,1000000000000000000LL);
+    else if(subtask==3)cout<<rnd.next("[1-9][0-9]{4999}");
+    else cout<<rnd.next("[1-9][0-9]{999999}");
+    cout<<'\n';
 }
