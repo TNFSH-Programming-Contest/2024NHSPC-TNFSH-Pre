97f45f527b36b7826bcfef761e23365ce1862b41
Samuel <138226245+GuoYuKai-SaMuEl@users.noreply.github.com>
2024-08-09 13:27:24 +0800

    Update correct.cpp

--- pF/solution/correct.cpp
+++ pF/solution/correct.cpp
@@ -1,10 +1,18 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+const int mod=1e9+9;
+int fp(int x,int y){
+    if(y==0)return 1;
+    if(y%2)return 1ll*fp(1ll*x*x%mod,y/2)*x%mod;
+    else return fp(1ll*x*x%mod,y/2);
+}
+int main(){
+    string s;
+    cin>>s;
+    long long int now=0;
+    for(char c:s){
+        now*=10;
+        now=(now+c-'0')%(mod-1);
+    }
+    cout<<fp(2,now);
 }
