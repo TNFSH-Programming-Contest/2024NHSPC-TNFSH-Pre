f291714430b98694e80058dabb22de570f5b2c71
Samuel <138226245+GuoYuKai-SaMuEl@users.noreply.github.com>
2024-08-17 18:46:21 +0800

    Update gen.cpp

--- pA/gen/gen.cpp
+++ pA/gen/gen.cpp
@@ -9,33 +9,29 @@ int main(int argc, char* argv[]) {
 	idx--;
 	long long int a,b,c,d,e,f;
 	if(subtask==1){
-		a=rnd.next(1,100000000),b=rnd.next(1,100000000),c=rnd.next(1,100000000);
-		d=rnd.next(max(1LL,a+b+c-200000000),min(100000000LL,a+b+c-2));
-		e=rnd.next(max(1LL,a+b+c-d-100000000),min(100000000LL,a+b+c-d-1));
-		f=a+b+c-d-e;
-		if(idx%3==1)if(a<d)swap(a,d);
-		else if(idx%3==2)if(a>d)swap(a,d);
-		else a=d;
-		if(idx/3%3==1)if(b<e)swap(b,e);
-		else if(idx/3%3==2)if(b>e)swap(b,e);
-		else b=e;
-		if(idx/9%3==1)if(c<f)swap(c,f);
-		else if(idx/9%3==2)if(c>f)swap(c,f);
-		else c=f;
+		if(idx%2)
+			a=rnd.next(1,100000000),b=rnd.next(1,100000000),c=rnd.next(1,100000000);
+			d=rnd.next(max(1LL,a+b+c-200000000),min(100000000LL,a+b+c-2));
+			e=rnd.next(max(1LL,a+b+c-d-100000000),min(100000000LL,a+b+c-d-1));
+			f=a+b+c-d-e;
+		}else{
+			long long int x[3],y[3];
+			for(int i=0;i<3;i++)x[i]=y[i]=rnd.next(1,100000000);
+			shuffle(x,x+3),shuffle(y,y+3);
+			a=x[0],b=x[1],c=x[2],d=y[0],e=y[1],f=y[2];
+		}
 	}else{
-		a=rnd.next(1000000000000000LL,1000000000000000000LL),b=rnd.next(1000000000000000LL,1000000000000000000LL),c=rnd.next(1000000000000000LL,1000000000000000000LL);
-		d=rnd.next(max(1000000000000000LL,a+b+c-2000000000000000000LL),min(1000000000000000000LL,a+b+c-2000000000000000LL));
-		e=rnd.next(max(1000000000000000LL,a+b+c-d-1000000000000000000LL),min(1000000000000000000LL,a+b+c-d-1000000000000000LL));
-		f=a+b+c-d-e;
-		if(idx%3==1)if(a<d)swap(a,d);
-		else if(idx%3==2)if(a>d)swap(a,d);
-		else a=d;
-		if(idx/3%3==1)if(b<e)swap(b,e);
-		else if(idx/3%3==2)if(b>e)swap(b,e);
-		else b=e;
-		if(idx/9%3==1)if(c<f)swap(c,f);
-		else if(idx/9%3==2)if(c>f)swap(c,f);
-		else c=f;
+		if(idx%2){
+			a=rnd.next(1000000000000000LL,1000000000000000000LL),b=rnd.next(1000000000000000LL,1000000000000000000LL),c=rnd.next(1000000000000000LL,1000000000000000000LL);
+			d=rnd.next(max(1000000000000000LL,a+b+c-2000000000000000000LL),min(1000000000000000000LL,a+b+c-2000000000000000LL));
+			e=rnd.next(max(1000000000000000LL,a+b+c-d-1000000000000000000LL),min(1000000000000000000LL,a+b+c-d-1000000000000000LL));
+			f=a+b+c-d-e;
+		}else{
+			long long int x[3],y[3];
+			for(int i=0;i<3;i++)x[i]=y[i]=rnd.next(1000000000000000LL,1000000000000000000LL);
+			shuffle(x,x+3),shuffle(y,y+3);
+			a=x[0],b=x[1],c=x[2],d=y[0],e=y[1],f=y[2];
+		}
 	}
 	cout<<a<<' '<<b<<' '<<c<<'\n'<<d<<' '<<e<<' '<<f<<'\n';
 	return 0;
