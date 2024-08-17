6b4ce98f1ae6c8baa2978af05e2571343a9e50a9
Same0620 <131766594+Same0620@users.noreply.github.com>
2024-08-17 15:14:44 +0800

    Update correct.cpp

--- pA/solution/correct.cpp
+++ pA/solution/correct.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+long long int a, b, c, d, e, f, tot, ans, tmp;
+void sol(){
+    cin >> a >> b >> c >> d >> e >> f;
+    tot = a+b+c;
+    ans = min(a, f)+min(b, d)+min(c, e);
+    tot-=ans;
+    tmp=min(a, f);
+    a-=tmp;f-=tmp;
+    tmp=min(b, d);
+    b-=tmp;d-=tmp;
+    tmp=min(c, e);
+    c-=tmp;e-=tmp;
+    if(a||d)tot-=a+d;
+    if(b||e)tot-=b+e;
+    if(c||f)tot-=c+f;
+    cout << abs(ans-tot) << endl;
+    return;
+}
+int main(){
+    int tt = 1;
+    while(tt--){
+        sol();
+    }
 }
