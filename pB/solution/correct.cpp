327207495e8aac8684ca0328ba03fc2c3f2f4b40
Same0620 <131766594+Same0620@users.noreply.github.com>
2024-08-17 15:56:00 +0800

    Update correct.cpp

--- pB/solution/correct.cpp
+++ pB/solution/correct.cpp
@@ -1,10 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+#define ll long long int
+ll n, c, k, a[200005];
+ll get(ll x){
+    int l=1, r=0;
+    ll tmp = 0, ans = 0;
+    for(;l<=n;l++){
+        while(tmp<x&&r<n)tmp+=a[++r];
+        if(tmp>=x)ans+=(n-r+1);
+    }
+    return ans;
+}
+void sol(){
+    cin >> n >> c >> k;
+    for(int i=1;i<=n;i++)cin >> a[i];
+    cout << get(c-k)-get(c+k) << endl;
+}
+int main(){
+    int tt = 1;
+    while(tt--){
+        sol();
+    }
 }
