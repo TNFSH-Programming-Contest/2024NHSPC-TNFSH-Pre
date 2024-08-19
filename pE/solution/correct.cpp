4bb4937ec2bda05e93ea18adce845b25fd0c5c6f
Samuel <138226245+GuoYuKai-SaMuEl@users.noreply.github.com>
2024-08-19 17:15:56 +0800

    Update correct.cpp

--- pE/solution/correct.cpp
+++ pE/solution/correct.cpp
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define E18 8000000000000000000LL
+#define E18 8000000000000000000ULL
 int a[(int)2e6+5];
-long long int dp[(int)2e6+5];
+unsigned long long int dp[(int)2e6+5];
 int main(){
-    cin.tie(0)->sync_with_stdio(0);
 	int n;
 	cin>>n;
 	for(int i=0;i<=n;i++){
@@ -12,7 +11,7 @@ int main(){
 		dp[i]=(i?E18:0);
 	}
 	for(int i=1;i<=n;i++){
-        long long int x=1;
+        unsigned long long int x=1;
         for(int j=1;j<=min(40,i);j++){
 		    if(x<=E18/a[i]/a[i-j]){
 		    	if(dp[i-j]+x*a[i]*a[i-j]<dp[i])dp[i]=dp[i-j]+x*a[i]*a[i-j];
